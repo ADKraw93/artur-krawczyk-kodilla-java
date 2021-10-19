732fe8bef2108e1dd2c54198125c617193c16508
Artur Krawczyk <artur.damian.krawczyk@gmail.com>
2021-10-19 17:51:03 +0200

    7.1 Excercises and solution added

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,30 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //Task 7.1 Solution
+        PoemBeautifier beautifier = new PoemBeautifier();
+        beautifier.beautify("Sample text", (text) -> "ABC" + text);
+        beautifier.beautify("Sample text", (text) -> text.toUpperCase());
+        beautifier.beautify("Sample text", (text) -> text.substring(0,6));
+        beautifier.beautify("Sample text", (text) -> text.substring(0,6) + text.toUpperCase() + text.substring(7,11));
+    }
+}
