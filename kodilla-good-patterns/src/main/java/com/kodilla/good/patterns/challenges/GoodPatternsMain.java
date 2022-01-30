b12e750317cb816ba958f5f90eb0bbed7bf6b123
Artur Krawczyk <artur.damian.krawczyk@gmail.com>
2022-01-30 22:06:43 +0100

    13.1. Commit zadania - nieudany

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/GoodPatternsMain.java
@@ -0,0 +1,11 @@
+package com.kodilla.good.patterns.challenges;
+
+public class GoodPatternsMain {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        movieStore.getTitles();
+    }
+
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,37 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+    public void getTitles(){
+
+        String listOfTitles = getMovies().entrySet().stream().
+                map(entry -> entry.getValue().stream().toString()).
+                collect(Collectors.joining("!", "", ""));
+
+        System.out.println(listOfTitles);
+    }
+}
